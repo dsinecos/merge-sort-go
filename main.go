d86fdb54ac83d1fe2b9bc47865443ca5adea6e5f
Divyanshu Singh <dsinecos@gmail.com>
2019-06-21 15:21:18 +0530

    add helper to test sorting on array of different sizes

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"runtime"
 
 	"github.com/dsinecos/merge-sort-go/concsort"
 	"github.com/dsinecos/merge-sort-go/seqsort"
 )
 
 func main() {
-	arrToSort := []int{3, 2, 4, 34, 5, 6, 2, 21}
-	fmt.Println("Array to sort ", arrToSort)
+	fmt.Println("Number of Logical CPUs ", runtime.NumCPU())
+	fmt.Println("GOMAXPROCS ", runtime.GOMAXPROCS(0))
+
+	// arrToSort := []int{3, 2, 4, 34, 5, 6, 2, 21}
+	arrToSort := []int{3, 2, 1}
+	// arrToSort := createArray(10000000)
+	// fmt.Println("Array to sort ", arrToSort)
+	// seqsort.Sort(arrToSort)
 	fmt.Println("Sequential sort", seqsort.Sort(arrToSort))
 
 	select {
-	case resultArray := <-concsort.Sort(arrToSort):
-		fmt.Println("Concurrent sort", resultArray)
+	case <-concsort.Sort(arrToSort):
+		// fmt.Println("Concurrent sort", resultArray)
+		fmt.Println("Maximum goroutines launched ", concsort.MaxGoroutine)
 	}
+}
+
+func createArray(size int) []int {
+	var array []int
+	for i := 0; i < size; i++ {
+		array = append(array, rand.Intn(10000000))
+	}
+	return array
 }
